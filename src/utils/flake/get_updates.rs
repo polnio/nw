dadc42603a3f463b4e8ba227eefa682fda6e9b3b
Po Co <polniolino@gmail.com>
2025-07-08 00:58:59 +0200

    Use rayon instead of manual multi-threaded iterator

--- src/utils/flake/get_updates.rs
+++ src/utils/flake/get_updates.rs
@@ -3,6 +3,7 @@ use super::remote::FlakeRemote;
 use crate::utils::args::ARGS;
 use crate::utils::errors;
 use anyhow::{Context as _, Result};
+use rayon::prelude::*;
 
 pub fn get_updates(flake: Option<&str>) -> Result<Vec<String>> {
     let metadata = FlakeMetadata::get(flake).context("Failed to fetch flake metadata")?;
@@ -13,35 +14,27 @@ pub fn get_updates(flake: Option<&str>) -> Result<Vec<String>> {
         .map(str::to_owned)
         .collect::<Vec<_>>();
 
-    let handles = metadata
+    let updates = metadata
         .locks
         .nodes
-        .into_iter()
+        .into_par_iter() // Parallel downloads
         .filter_map(|(name, node)| {
             if !inputs.contains(&name) {
                 return None;
             }
             let flake: String = node.original?.try_into().ok()?;
-            let handle = std::thread::spawn(move || FlakeRemote::get(&flake));
-            Some((name, node.locked, handle))
-        })
-        .collect::<Vec<_>>();
-
-    let updates = handles
-        .into_iter()
-        .filter_map(|(name, locked, handle)| {
-            let remote = handle
-                .join()
-                .unwrap()
-                .map_err(|e| {
+            let remote = match FlakeRemote::get(&flake) {
+                Ok(remote) => remote,
+                Err(e) => {
                     if !ARGS.quiet {
                         errors::print_error(e);
                     }
-                })
-                .ok()?;
-            (locked?.hash != remote.hash).then_some(name)
+                    return None;
+                }
+            };
+            (node.locked?.hash != remote.hash).then_some(name)
         })
-        .collect();
+        .collect::<Vec<_>>();
 
     Ok(updates)
 }
